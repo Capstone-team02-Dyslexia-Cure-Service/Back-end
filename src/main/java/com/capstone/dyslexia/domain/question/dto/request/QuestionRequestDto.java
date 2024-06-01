e48d53ffeb559a02a8b4aceaa38cdd17e145219f
Dustbox <sanggae4133@naver.com>
2024-06-01 22:43:24 +0900

    fix: 전반적인 불필요 코드 정리

--- src/main/java/com/capstone/dyslexia/domain/question/dto/request/QuestionRequestDto.java
+++ src/main/java/com/capstone/dyslexia/domain/question/dto/request/QuestionRequestDto.java
@@ -1,25 +1,9 @@
 package com.capstone.dyslexia.domain.question.dto.request;
 
-import com.capstone.dyslexia.domain.question.domain.QuestionType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 public class QuestionRequestDto {
-
-    @Getter
-    public static class Create {
-        @NotNull(message = "Question Type은 필수입니다.")
-        private QuestionType questionType;
-
-        @NotBlank(message = "Question 내용은 비어서는 안 됩니다.")
-        private String content;
-
-        private String pronunciationFilePath;
-
-        private String videoPath;
-    }
 
     @Getter
     @Builder
