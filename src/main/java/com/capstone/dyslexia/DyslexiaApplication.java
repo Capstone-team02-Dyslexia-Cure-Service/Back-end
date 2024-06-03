0ece622b3c6cd27d4dd12c62cf354aaf4212adfd
Dustbox <sanggae4133@naver.com>
2024-06-04 04:01:53 +0900

    fix: swagger https 대응 cors 에러 수정

--- src/main/java/com/capstone/dyslexia/DyslexiaApplication.java
+++ src/main/java/com/capstone/dyslexia/DyslexiaApplication.java
@@ -1,6 +1,8 @@
 package com.capstone.dyslexia;
 
 import com.capstone.dyslexia.domain.level.config.LevelRangeConfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -11,6 +13,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableConfigurationProperties(LevelRangeConfig.class)
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@OpenAPIDefinition(servers = {
+		@Serve
+		gr(url = "/", description = "Default Server URL")
+})
 public class DyslexiaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/capstone/dyslexia/global/config/openApi/OpenApiConfig.java
+++ src/main/java/com/capstone/dyslexia/global/config/openApi/OpenApiConfig.java
@@ -1,7 +1,12 @@
 package com.capstone.dyslexia.global.config.openApi;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
